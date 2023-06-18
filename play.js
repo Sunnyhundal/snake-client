2dcfa79cc13589b0ce09c40c71d9989718b8f13c
Sunny Hundal <amandip.h@gmail.com>
2023-06-17 17:43:29 -0700

    Add setupInput to detect input cmds

--- play.js
+++ play.js
@@ -1,4 +1,26 @@
 const { connect } = require('./client');
 
+//Provides user a prompt that they're leaving the game and exits the game
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    console.log("~#~ leaving game ~#~");
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+
 console.log("Connecting ...");
 connect();
+setupInput();
