42bb9561a2b398605f033c76dfc8f0f7e8fb76bb
Sunny Hundal <amandip.h@gmail.com>
2023-06-17 18:51:36 -0700

    Move code for input detection to input.js

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
 const { connect } = require('./client');
-
-//Provides user a prompt that they're leaving the game and exits the game
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    console.log("~#~ leaving game ~#~");
-    process.exit();
-  }
-};
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-
-
+const { setupInput } = require('./input')
 
 console.log("Connecting ...");
 connect();
+
 setupInput();
+
