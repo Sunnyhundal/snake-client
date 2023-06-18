3ff7aaaac4dc9f16466452701d13da0fa5efb91b
Sunny Hundal <amandip.h@gmail.com>
2023-06-17 21:44:35 -0700

    Change movement to constants and ESLINT

--- input.js
+++ input.js
@@ -1,25 +1,32 @@
+const { UP, LEFT, DOWN, RIGHT, MSGONE, MSGTWO} = require('./constants');
+
 let connection;
 
+
 //Provides user a prompt that they're leaving the game and exits the game
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log("~#~ leaving game ~#~");
     process.exit();
   }
-  if(key === 'w'){
-    connection.write("Move: up");
-  }else if (key === 'a'){
-    connection.write("Move: left");
-  }else if (key === 's'){
-    connection.write("Move: down");
-  }else if (key === 'd'){
-    connection.write("Move: right");
+  if (key === 'w') {
+    connection.write(UP);
+  } else if (key === 's') {
+    connection.write(DOWN);
+  } else if (key === 'a') {
+    connection.write(LEFT);
+  } else if (key === 'd') {
+    connection.write(RIGHT);
+  } else if (key === 'r') {
+    connection.write(MSGONE);
+  } else if (key === 'g') {
+    connection.write(MSGTWO);
   }
     
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
