130ff0047b065bdb4755277e502042be3e19fc9f
Sunny Hundal <amandip.h@gmail.com>
2023-06-17 19:13:59 -0700

    Add logic to receive movement input

--- input.js
+++ input.js
@@ -1,13 +1,26 @@
+let connection;
+
 //Provides user a prompt that they're leaving the game and exits the game
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     console.log("~#~ leaving game ~#~");
     process.exit();
   }
+  if(key === 'w'){
+    console.log("Move: up");
+  }else if (key === 'a'){
+    console.log("Move: left");
+  }else if (key === 's'){
+    console.log("Move: down");
+  }else if (key === 'd'){
+    console.log("Move: right");
+  }
+    
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
